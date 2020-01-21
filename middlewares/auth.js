16cc7829d3ad23a4aedf2a46c00f9e7915c53996
Максим Маслов <flyover31@yandex.ru>
2020-01-22 00:38:26 +0300

    правки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,21 +2,24 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const JWT_SECRET = 'some-secret-key';
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
       .send({ message: 'Доступ запрещен. Необходима авторизация' });
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     return res
       .status(401)
       .send({ message: 'Доступ запрещен. Необходима авторизация' });
   }
-  req.user = payload;
-  next();
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
 };
